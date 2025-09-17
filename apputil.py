17333aa00380de9818437f9c01147b9fb882ed9e
aitiern <aitiern@iu.edu>
2025-09-16 20:38:03 -0400

    Update for exercise 1

--- apputil.py
+++ apputil.py
@@ -2,4 +2,9 @@ import seaborn as sns
 import pandas as pd
 
 
-# update/add code below ...
+# Exercise 1
+# create a pandas Series of Fibonacci numbers up to n
+def fib_series(n):
+    values = [fib(i) for i in range(n+1)]
+    return pd.Series(values, index=range(n+1), name="Fibonacci")
+
