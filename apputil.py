f3f6710f6dcc70c7139221a2e28ff29785b4388a
aitiern <aitiern@iu.edu>
2025-09-16 20:39:55 -0400

    Update exercise 2

--- apputil.py
+++ apputil.py
@@ -8,3 +8,12 @@ def fib_series(n):
     values = [fib(i) for i in range(n+1)]
     return pd.Series(values, index=range(n+1), name="Fibonacci")
 
+# Exercise 2
+
+def to_binary(n):
+    # Base case: 0 and 1
+    if n < 2:
+        return str(n)
+    else:
+        # Recursive case: divide by 2, then append remainder
+        return to_binary(n // 2) + str(n % 2)
